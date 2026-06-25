9679c217fdd2f10c3c03ba14b432620b61392b47
Bell <isabelaccilio@gmail.com>
2026-06-25 03:49:16 -0500

    Delete C++ (1) directory

--- C++ (1)/13.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-    long double suma=0, producto=1;
-    for(int i=20;i<=400;i+=2){suma+=i;producto*=i;}
-    cout << "Suma de pares (20-400): " << suma << endl;
-    cout << "Producto de pares (20-400): " << producto << endl;
-    return 0;
-}
--- C++ (1)/3.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-    int suma = 0;
-    for(int i = 1; i <= 20; i++) suma += i * 3;
-    cout << "Suma de los 20 primeros múltiplos de 3: " << suma << endl;
-    return 0;
-}
